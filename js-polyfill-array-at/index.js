894f0e79e661f4bad7328519b087f742716c2b78
ram <chry.rgm@gmail.com>
2024-02-06 16:46:12 +0545

    solution

--- js-polyfill-array-at/index.js
+++ js-polyfill-array-at/index.js
@@ -9,6 +9,14 @@ if (!Array.prototype.at) {
 // However, for this exercise, we'll define it as a regular function.
 
 export function arrayAt(arr, index) {
-    // TODO: Implement the functionality of Array.at
-  }
+  // TODO: Implement the functionality of Array.at
+
+  // check if the index is out of bounds of the array
+  if (index < 0 && index >= arr.length) return undefined;
+
+  // If the index is negative, calculate the positive index and return the corresponding element
+  if (index < 0 && index >= -arr.length) return arr[arr.length + index]
+
   
+  return arr[index];
+}
--- js-polyfill-array-at/index.test.js
+++ js-polyfill-array-at/index.test.js
@@ -2,4 +2,15 @@ import { arrayAt } from "./index.js";
 
 describe("Array.at Polyfill", () => {
   // Write your tests here
+  const MOCK_ARRAY = ["first","second","third","fourth","fifth","sixth","seventh","eight","ninth"];
+  test("The method returns the correct item for a positive index.",()=>{
+    expect(arrayAt(MOCK_ARRAY,3)).toBe("fourth");
+  });
+
+  test("The method returns the correct item for a negative index.",()=>{
+    expect(arrayAt(MOCK_ARRAY,-3)).toBe("seventh");
+  });
+  test("The method returns undefined for an index that is out of the array's bounds.",()=>{
+    expect(arrayAt(MOCK_ARRAY,-30)).toBe(undefined);
+  });
 });
