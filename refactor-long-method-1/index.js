06e15df911f87d55698bf7f26dfad99eb5503d5f
ram <chry.rgm@gmail.com>
2024-02-06 23:40:45 +0545

    solution

--- refactor-long-method-1/index.js
+++ refactor-long-method-1/index.js
@@ -1,16 +1,11 @@
-export const processSalesData = () => {
-    // Filter valid sales records
-    let validSales = [];
-    for (let i = 0; i < salesData.length; i++) {
-      if (salesData[i].amount > 0) {
-        validSales.push(salesData[i]);
-      }
-    }
-  
+export const processSalesData = (salesData) => {
+    
     // Calculate total sales
     let totalSales = 0;
-    for (let i = 0; i < validSales.length; i++) {
-      totalSales += validSales[i].amount;
+    for (let i = 0; i < salesData.length; i++) {
+        if (salesData[i].amount > 0) {
+            totalSales += salesData[i].amount;
+        }
     }
   
     return totalSales;
--- refactor-long-method-1/index.test.js
+++ refactor-long-method-1/index.test.js
@@ -1,4 +1,21 @@
 import { processSalesData } from "./index";
 
 // WRITE ALL TESTS BEFORE YOU START REFACTORING
-describe("Process Sales Data", () => {});
+describe("Process Sales Data", () => {
+    test('The function correctly calculates the total sales for valid sales records', () => { 
+        
+        const mock_data = [{amount:1000},{amount:343343},{amount:3434343}];
+        expect(processSalesData(mock_data)).toBe(3778686);
+     });
+
+     test('The function ignores sales records with negative amounts.', () => { 
+        const mock_data = [{amount:1000},{amount:-343343},{amount:3434343}];
+        expect(processSalesData(mock_data)).toBe(3435343);
+
+      });
+
+      test('The function handles an empty array without errors.', () => { 
+        expect(processSalesData([])).toBe(0);
+
+       });
+});
