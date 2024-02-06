203f36d87d5e2cac154f980ecac8c76a567a62b1
ram <chry.rgm@gmail.com>
2024-02-06 16:17:25 +0545

    js-polyfill-array-at question

---
+++ js-polyfill-array-at/index.js
@@ -0,0 +1,14 @@
+// Normally, a polyfill would be implemented as part of the prototype of a global object.
+/* Something like the below
+if (!Array.prototype.at) {
+  Array.prototype.at = function(index) {
+    // Implementation goes here.
+  };
+} 
+*/
+// However, for this exercise, we'll define it as a regular function.
+
+export function arrayAt(arr, index) {
+    // TODO: Implement the functionality of Array.at
+  }
+  
---
+++ js-polyfill-array-at/index.test.js
@@ -0,0 +1,5 @@
+import { arrayAt } from "./index.js";
+
+describe("Array.at Polyfill", () => {
+  // Write your tests here
+});
